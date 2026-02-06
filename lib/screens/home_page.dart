d6ce146686540e8e18c3317e349ec8e189211566
Milca Sanchez <sanchezmilca05@gmail.com>
2026-02-06 14:09:13 -0600

    AddBar added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:donut_app/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,9 +9,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+ List<Widget> myTabs = [
+    //donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png', iconName: 'Donut'),
+    //burger tab
+    const MyTab(iconPath: 'lib/icons/burger.png', iconName: 'Burger'),
+    //smoothie tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png', iconName: 'Smoothie'),
+    //pancake tab
+    const MyTab(iconPath: 'lib/icons/pancakes.png', iconName: 'Pancake'),
+    //pizza tab
+    const MyTab(iconPath: 'lib/icons/pizza.png', iconName: 'Pizza'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         // Icono de la izquierda
@@ -25,14 +41,30 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         //1. Texo principal
-
+        children: [Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: Row(
+            children: [
+              Text('I want to ', style: TextStyle(fontSize: 24)),
+              Text(
+                'Eat',
+               style: TextStyle(
+                //Tamaño de la letra
+                fontSize: 24,
+                //Negritas
+                fontWeight: FontWeight.bold,
+                //Subrayado
+                decoration: TextDecoration.underline,
+                )
+              ),
+            ],
+          ),
+        ),
         //2. Pestañas (TabBar)
-
-        //3. Contenido (TabBarView)
-
-        // 4. Carrito (Cart)
-        
+         TabBar(tabs: myTabs),
+        ]
       ),
+    ),
     );
   }
 }
